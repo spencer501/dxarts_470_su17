8b9c708aea7f8f52851d5c2c97ef1a287a396093
Spencer Pease <spencer.p.816@gmail.com>
2017-07-22 16:58:14 -0700

    Set up `arduino_code.ino`

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,1 +1,19 @@
+/*
+ * Spencer Pease
+ * DXARTS 470
+ * 
+ * Draft 1:
+ * Create an array of LEDs that are controlled via external
+ * serial communication
+ */
+
+void setup() {
+  
+
+}
+
+void loop() {
+  
+
+}
 
