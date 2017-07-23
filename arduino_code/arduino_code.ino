f30640d1f7fb45316807c9d87ed8229a70b5c6df
Spencer Pease <spencer.p.816@gmail.com>
2017-07-22 17:27:20 -0700

    Add basic LED lighting functionality
    All LEDs will turn on as for as long as the button is pressed

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -7,13 +7,46 @@
  * serial communication
  */
 
-void setup() {
-  
+// Define constants
+#define LED_PIN_R 6
+#define LED_PIN_Y 5
+#define LED_PIN_G 4
+#define LED_PIN_B 3
 
+#define BUTTON_PIN 2
+
+
+void setup() {
+
+  pinMode(LED_PIN_R, OUTPUT);
+  pinMode(LED_PIN_Y, OUTPUT);
+  pinMode(LED_PIN_G, OUTPUT);
+  pinMode(LED_PIN_B, OUTPUT);
+
+  pinMode(BUTTON_PIN, INPUT);
+  
 }
 
 void loop() {
-  
+
+  int buttonActive = digitalRead(BUTTON_PIN);
+
+  if (buttonActive == HIGH) {
+    
+    digitalWrite(LED_PIN_R, HIGH);
+    digitalWrite(LED_PIN_Y, HIGH);
+    digitalWrite(LED_PIN_G, HIGH);
+    digitalWrite(LED_PIN_B, HIGH);
+    delay(100);
+  }
+  else {
+
+    digitalWrite(LED_PIN_R, LOW);
+    digitalWrite(LED_PIN_Y, LOW);
+    digitalWrite(LED_PIN_G, LOW);
+    digitalWrite(LED_PIN_B, LOW);
+    delay(100);
+  }
 
 }
 
