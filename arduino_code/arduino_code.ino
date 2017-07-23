710b328e5c4f5ff6d7abbe4a422bba6a8b2d021e
Spencer Pease <spencer.p.816@gmail.com>
2017-07-22 18:37:55 -0700

    Refactor code
    - Use arrays to reduce variable declarations
    - Remove if-else statement with better logic

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -8,45 +8,29 @@
  */
 
 // Define constants
-const int LED_R = 6;
-const int LED_Y = 5;
-const int LED_G = 4;
-const int LED_B = 3;
+const int LED[] = {3, 4, 5, 6};
+const int TOTAL_LED = 4;
 
 const int BUTTON = 2;
 
 
 void setup() {
 
-  pinMode(LED_R, OUTPUT);
-  pinMode(LED_Y, OUTPUT);
-  pinMode(LED_G, OUTPUT);
-  pinMode(LED_B, OUTPUT);
+  for (int i=0; i < TOTAL_LED; i++) {
+    pinMode(LED[i], OUTPUT);
+  }
 
   pinMode(BUTTON, INPUT);
-  
 }
 
 void loop() {
 
-  int buttonActive = digitalRead(BUTTON);
+  int buttonState = digitalRead(BUTTON);
 
-  if (buttonActive == HIGH) {
-    
-    digitalWrite(LED_R, HIGH);
-    digitalWrite(LED_Y, HIGH);
-    digitalWrite(LED_G, HIGH);
-    digitalWrite(LED_B, HIGH);
-    delay(100);
-  }
-  else {
-
-    digitalWrite(LED_R, LOW);
-    digitalWrite(LED_Y, LOW);
-    digitalWrite(LED_G, LOW);
-    digitalWrite(LED_B, LOW);
-    delay(100);
+  for (int i=0; i < TOTAL_LED; i++) {
+    digitalWrite(LED[i], buttonState);
   }
 
+  delay(100);
 }
 
