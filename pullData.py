b7005e8f20a7ccdfdeaf167ebbae078479afd317
Spencer Pease <spencer.p.816@gmail.com>
2017-07-23 17:34:59 -0700

    Add functionality to pull recent changes from Wikipedia
    Code based off of work from Wikimedia[1]. Currently prints size
    changes of edited pages
    
    [1]: https://wikitech.wikimedia.org/wiki/EventStreams#Python

--- pullData.py
+++ pullData.py
@@ -1,3 +1,33 @@
 """
 File for pulling in data and remapping for export
+Modeled after: https://wikitech.wikimedia.org/wiki/EventStreams
 """
+
+# Import libraries
+import json
+from sseclient import SSEClient as EventSource
+
+# Parameters for stream source
+url = 'https://stream.wikimedia.org/v2/stream/recentchange'
+wiki = 'enwiki'
+changeType = 'edit'
+
+for event in EventSource(url):
+    
+    if (event.event == 'message'):
+        
+        try:
+            change = json.loads(event.data)
+        except ValueError:
+            continue
+        
+        if (change['wiki'] == wiki and
+            change['type'] == changeType):
+            
+            oldSize = change['revision']['old']
+            newSize = change['revision']['new']
+            sizeDiff = newSize - oldSize
+            
+            print('Old size:', oldSize, 
+                  'New size:', newSize, 
+                  'Diff:', sizeDiff)
