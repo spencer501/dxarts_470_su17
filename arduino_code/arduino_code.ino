8c0e831e2ab951501a93bad07061e83741e1019a
Spencer Pease <spencer.p.816@gmail.com>
2017-07-30 18:33:10 -0700

    Rework Arduino code to work with solenoids
    References issues #4 and #5

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,17 +1,15 @@
 /*
- * Spencer Pease
- * DXARTS 470
- * 
- * Draft 1:
- * Create an array of LEDs that are controlled via external
- * serial communication
- */
+   Spencer Pease
+   DXARTS 470
+
+   Draft 2:
+   Create an array of solenoids that are controlled via external
+   serial communication
+*/
 
 // Define constants
-const int LED[] = {3, 4, 5, 6};
-const int TOTAL_LED = 4;
-
-const int TEST_LED = 13;
+const int SOLENOID[] = {3, 4, 5, 6};
+const int TOTAL_SOLENOID = 4;
 
 const int BUTTON = 2;
 
@@ -20,11 +18,9 @@ void setup() {
 
   Serial.begin(9600);
 
-  for (int i=0; i < TOTAL_LED; i++) {
-    pinMode(LED[i], OUTPUT);
+  for (int i = 0; i < TOTAL_SOLENOID; i++) {
+    pinMode(SOLENOID[i], OUTPUT);
   }
-
-  pinMode(TEST_LED, OUTPUT);
 
   pinMode(BUTTON, INPUT);
 }
@@ -33,41 +29,35 @@ void loop() {
 
   int buttonState = digitalRead(BUTTON);
 
-  if (Serial.available() > 0) {
-    digitalWrite(TEST_LED, HIGH);
-  }
-  
   if (buttonState == LOW) {
-    
-    flash(3, 100);
-  } 
+
+    cascade(1, 100);
+  }
   else if (Serial.available() > 0) {
-    
-    int activeLED = Serial.read() - 1;
-    
-    digitalWrite(LED[activeLED], HIGH);
+
+    int activeSolenoid = Serial.read() - 1;
+
+    digitalWrite(SOLENOID[activeSolenoid], HIGH);
     delay(175);
-    digitalWrite(LED[activeLED], LOW);
+    digitalWrite(SOLENOID[activeSolenoid], LOW);
     delay(100);
 
-    Serial.print(activeLED + 1);
+    Serial.print(activeSolenoid + 1);
   }
 }
 
 
-void flash(int cycles, int duration) {
+void cascade(int cycles, int duration) {
 
-  for (int i=0; i < cycles; i++) {
-  
-    for (int i=0; i < TOTAL_LED; i++) {
-      digitalWrite(LED[i], HIGH);
+  for (int i = 0; i < cycles; i++) {
+
+    for (int i = 0; i < TOTAL_SOLENOID; i++) {
+
+      digitalWrite(SOLENOID[i], HIGH);
+      delay(duration);
+      digitalWrite(SOLENOID[i], LOW);
+      delay(duration);
     }
-    delay(duration);
-    
-    for (int i=0; i < TOTAL_LED; i++) {
-      digitalWrite(LED[i], LOW);
-    }
-    delay(duration);
   }
 }
 
