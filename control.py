290d47b33033cad80aa626c6c1afbf54a54106e3
Spencer Pease <spencer.p.816@gmail.com>
2017-08-11 02:13:03 -0700

    Replicate functionality of `control_old.py` with more modular code
    Cod eis now seperated out into `control.py` and `wikiRead.py`

--- control.py
+++ control.py
@@ -1,16 +1,24 @@
 import time
 import serial
+import wikiRead
 
 # Create Serial object
 port = 'COM5'
 baudrate = 9600
 ser = serial.Serial(port, baudrate, timeout = 1)
 
+print('Trying to open serial communication on ' + ser.name)
+
 # Make sure Serial port is open and give it time to reset the connection
 if ser.isOpen():
-     print(ser.name + ' is open...')
-     time.sleep(3)
-     
+    time.sleep(3)
+    print(ser.name + ' is open...')
+    time.sleep(1)
+
+# Start streaming wiki changes
+wr = wikiRead.WikiReader()
+
+wr.streamChanges(ser, "end")
  
 
 # Close the Serial connection so it can be used by other processes
